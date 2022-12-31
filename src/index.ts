d28b24a61a52c3349f3a454154143701c88b5f18
sasika bandara <sasika@grabtech.com>
2022-12-31 10:39:03 +0530

    resolve mocypress build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import {Button} from "../node_modules/@mocypress/mocypress-ui-core/dist/Elements_Package"
+import {Button} from "@mocypress/mocypress-ui-core/dist/Elements_Package"
 export { Button }
