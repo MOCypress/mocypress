53d1f0b1585f59d9c2c1fd53edb92b2320a70661
sasika bandara <sasika@grabtech.com>
2022-12-31 10:26:43 +0530

    resolve mocypress build

--- index.ts
+++
@@ -1,8 +0,0 @@
-
-describe("== Sample Reflect Code For Dynamic Loading ==",function(){
- it("Method Access Demo", function(){
-
-     console.log("Hello World !")
-
- })
-});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import {Button} from "../node_modules/@mocypress/mocypress-ui-core/dist/Elements_Package"
+export { Button }
